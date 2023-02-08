0adcedfe33cceb9056b6bca300e85e0a99dda671
Friosn <franz.rios.nino@gmail.com>
2023-02-08 11:03:32 +0100

    Added new script test:watch, added a new test to check if notes come as json

--- server/src/__test__/actors.test.js
+++ server/src/__test__/actors.test.js
@@ -1,14 +1,28 @@
 const supertest = require('supertest');
-const app = require('../../index');
+const { app, server } = require('../../index');
 const mongoose = require('mongoose');
 const Actor = require('../api/models/actors.model');
 
 //Primera prueba
+const api = supertest(app);
 
-describe('GET /actors', () => {
+test('Notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+  server.close();
+});
+
+/* describe('GET /actors', () => {
   it('should get all actors', async () => {
     const res = await request(app).get('/api/actors');
     expect(res.status).toBe(200);
     expect(res.body).toEqual(mockDB.Actor.endpoints[0].response);
   });
 });
+ */
